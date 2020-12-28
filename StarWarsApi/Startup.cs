661274344cc8ae90ab763d0446bb6eea6ff47beb
Erin Stevenson <estevenson_94@live.com>
2020-12-28 13:17:41 +0000

    Remove template api files

--- StarWarsApi/Startup.cs
+++ StarWarsApi/Startup.cs
@@ -26,12 +26,7 @@ namespace StarWarsApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "StarWarsApi", Version = "v1" });
-            });
+            services.AddControllers(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +35,7 @@ namespace StarWarsApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StarWarsApi v1"));
+
             }
 
             app.UseHttpsRedirection();
